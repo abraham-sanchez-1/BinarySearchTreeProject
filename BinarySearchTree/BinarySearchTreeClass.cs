599622af7e82ccb61e095fec3621b32808cd4d07
Abraham Sanchez <sanchez95san@gmail.com>
2020-01-30 15:16:29 -0600

    innit

---
+++ BinarySearchTree/BinarySearchTreeClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BinarySearchTreeClass
+    {
+        //member variable
+        Node start;
+        Node recent;
+
+        //constructor
+        public BinarySearchTreeClass()
+        {
+            start = null;
+          
+        }
+        //member method
+        public void InsertAtEnd(int data)
+        {
+            if (start == null)
+            {
+                start = new Node(data);
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class Node
+    {
+        //member variable
+        public int data { get; set; }
+        public Node left { get; set; }
+        public Node right { get; set; }
+
+        //constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+        //member method
+
+    }
+}
